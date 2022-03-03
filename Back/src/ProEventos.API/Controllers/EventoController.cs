d8ef77288f91155b0bda92bc9db4a6f20a14e135
César <paulo1988cesar@msn.com>
2022-03-03 10:22:31 -0300

    1º Checkin

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento
+            {
+                EventoId = 1,
+                Tema = "Curso Angular .NET 5",
+                DataEvento = DateTime.UtcNow.AddDays(60),
+                Local = "Belo Horizonte",
+                Lote = "1º Lote",
+                QtdPessoas = 250
+            };
+        }
+
+        [HttpGet("GetAll")]
+        public IEnumerable<Evento> GetAll()
+        {
+            List<Evento> eventos = new List<Evento>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                eventos.Add(new Evento
+                {
+                    EventoId = i += 1,
+                    Tema = "Curso Angular .NET 5",
+                    DataEvento = DateTime.UtcNow.AddDays(60 + i),
+                    Local = "Belo Horizonte",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250
+                });
+            }
+            return eventos.AsEnumerable();
+        }
+
+        [HttpGet("{id}")]
+        public Evento Get(int id)
+        {
+            List<Evento> eventos = new List<Evento>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                eventos.Add(new Evento
+                {
+                    EventoId = i += 1,
+                    Tema = "Curso Angular .NET 5",
+                    DataEvento = DateTime.UtcNow.AddDays(60 + i),
+                    Local = "Belo Horizonte",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250
+                });
+            }
+            return eventos.Where(c => c.EventoId == id).FirstOrDefault();
+        }
+    }
+}
